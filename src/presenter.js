b40123fa2815a09289a68517144d640d43728c51
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-02-29 00:51:18 -0400

    Implementamos mostrar en pantalla el precio neto mas un error si los valores son negativos

--- src/presenter.js
+++ src/presenter.js
@@ -5,7 +5,8 @@ const cantidad = document.querySelector("#cantidad");
 const precio = document.querySelector("#precio");
 const estado = document.querySelector("#estado");
 const form = document.querySelector("#totalizar-form");
-const div = document.querySelector("#resultado-div"); 
+const div = document.querySelector("#resultado-div");
+const div2 = document.querySelector("#resultado-div2");  
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,4 +14,10 @@ form.addEventListener("submit", (event) => {
   //const secondNumber = Number.parseInt(second.value);
   const totalizar = new totalizador(cantidad.value, precio.value, estado.value);
   div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() +" | Precio: " + totalizar.get_precio() + " | Estado: " + totalizar.get_estado() +"</p>";
+  
+  if(totalizar.get_cantidad() != null && totalizar.get_precio() != null){  
+    div2.innerHTML = "<p>Precio neto: " + totalizar.get_precio_neto() + "</p>";
+  }else
+    div2.innerHTML = "<p>ERROR! La cantidad o el precio es negativo.</p>";
+  
 });
