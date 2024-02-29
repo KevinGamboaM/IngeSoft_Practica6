1e40c482cb2f9100ee3cf64c218f0fff4a2f8f46
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-02-29 01:29:46 -0400

    Implementamos mostrar en pantalla todos los descuentos

--- src/presenter.js
+++ src/presenter.js
@@ -7,6 +7,7 @@ const estado = document.querySelector("#estado");
 const form = document.querySelector("#totalizar-form");
 const div = document.querySelector("#resultado-div");
 const div2 = document.querySelector("#resultado-div2");  
+const div3 = document.querySelector("#resultado-div3");  
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -17,7 +18,10 @@ form.addEventListener("submit", (event) => {
   
   if(totalizar.get_cantidad() != null && totalizar.get_precio() != null){  
     div2.innerHTML = "<p>Precio neto: " + totalizar.get_precio_neto() + "</p>";
-  }else
+  }else{
     div2.innerHTML = "<p>ERROR! La cantidad o el precio es negativo.</p>";
+  }
+  div3.innerHTML = "<p>Descuento(" + parseInt(totalizar.get_descuento()*100) + "%)" + totalizar.get_precio_neto()*totalizar.get_descuento() + "</p>";
+    
   
 });
